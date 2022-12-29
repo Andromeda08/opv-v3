9ca4e7ed78fe0781f12744850a58de71b2ed62a2
Andromeda08 <vkovacsbalazs26@gmail.com>
2022-12-29 18:26:56 +0100

    Adjust canonical url.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,14 +22,14 @@ class MyDocument extends Document {
           <meta name="keywords" content="osu osu! osu!game osugame player profile viewer"/>
 
           <meta property='og:type' content='website' />
-          <meta property='og:url' content='https://opv.tsukiakari.studio' />
+          <meta property='og:url' content='https://osu.tsukiakari.hu/' />
           <meta property='og:title' content='osu!pv' />
           <meta property='og:description' content='View your and other players osu! profiles.' />
           <meta property='og:image' content='/icon.png' />
           <meta property="og:image:width" content="64" />
           <meta property="og:image:height" content="64" />
 
-          <link rel="canonical" href="https://opv.tsukiakari.studio" />
+          <link rel="canonical" href="https://osu.tsukiakari.hu" />
 
           <link rel="shortcut icon" href="/icon.png"  type="image/png" />
           <link rel="apple-touch-icon" href="/icon.png" />
